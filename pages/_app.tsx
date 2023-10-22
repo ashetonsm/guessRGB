4660bb76446cb66a432b9cd6f9ece164ee5276fa
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2023-10-22 15:54:05 -0500

    added firebase info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
 import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 import Layout from '@/components/layout';
-import '@/styles/custom.scss'
+import '@/styles/custom.scss';
+import config from '@/src/lib/firebase/config';
 import { GameProvider } from '@/context/GameContext';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Initialize Firebase
+const app = initializeApp(config);
+const analytics = getAnalytics(app);
+
 export default function MyApp({
+
+  
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps) {
